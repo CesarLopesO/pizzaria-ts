7312c02ef6385e600905a620f2282297fec9bac9
Cesar <cesarlopesg2@gmail.com>
2022-11-23 16:50:31 -0300

    23/11/22

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,24 @@
 import express, {Request,Response,NextFunction} from 'express'
+import 'express-async-errors';
+import { router } from './routes'
 const app=express();
+app.use(express.json())
 
-app.listen(3333,()=> console.log("TUDO OK"))
+app.use(router);
+app.use((err:Error,req: Request,res:Response,next:NextFunction)=>{
+if(err instanceof Error){ 
+    
+    return res.status(400).json({
+
+
+    })
+}
+return res.status(500).json({
+     status:'error'
+    ,message:'Internal server error;'})
+
+ }
+)
+
+app.listen(3000,()=> console.log("TUDO OK"))
+
